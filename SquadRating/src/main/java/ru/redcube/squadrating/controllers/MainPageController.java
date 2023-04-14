43714127d899e7004303d6ad1ff2a03b38aaa83f
vudovenko <vladislav.udovenko0@yandex.ru>
2023-04-15 00:08:15 +0500

    Created MainPageController

---
+++ SquadRating/src/main/java/ru/redcube/squadrating/controllers/MainPageController.java
@@ -0,0 +1,13 @@
+package ru.redcube.squadrating.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainPageController {
+
+    @GetMapping("/")
+    public String getMainPage() {
+        return "/index";
+    }
+}
