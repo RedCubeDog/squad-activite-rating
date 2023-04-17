781c05d65bc9f71740a8ff00f2344aa27708b565
vudovenko <vladislav.udovenko0@yandex.ru>
2023-04-17 23:34:11 +0500

    changed data.sql

--- SquadRating/src/main/resources/data.sql
+++ SquadRating/src/main/resources/data.sql
@@ -1,10 +1,10 @@
 insert into squad (id, name) values (1, 'squad1');
 
 insert into app_user (id, email, first_name, last_name, login, patronymic, squad_role, state, squad_id)
-values (1, 'test_email1@email.com', 'firstname1', 'last_name1', 'login1', 'patronymic1', 'squad_role1', 'state1', 1);
+values (1, 'test_email1@email.com', 'firstname1', 'last_name1', 'login1', 'patronymic1', 'CANDIDATE', 'BASIC_STATE', 1);
 insert into app_user (id, email, first_name, last_name, login, patronymic, squad_role, state, squad_id)
-values (2, 'test_email2@email.com', 'firstname2', 'last_name2', 'login2', 'patronymic2', 'squad_role2', 'state2', 1);
+values (2, 'test_email2@email.com', 'firstname2', 'last_name2', 'login2', 'patronymic2', 'CANDIDATE', 'BASIC_STATE', 1);
 insert into app_user (id, email, first_name, last_name, login, patronymic, squad_role, state, squad_id)
-values (3, 'test_email3@email.com', 'firstname3', 'last_name3', 'login3', 'patronymic3', 'squad_role3', 'state3', 1);
+values (3, 'test_email3@email.com', 'firstname3', 'last_name3', 'login3', 'patronymic3', 'CANDIDATE', 'BASIC_STATE', 1);
 insert into app_user (id, email, first_name, last_name, login, patronymic, squad_role, state, squad_id)
-values (4, 'test_email4@email.com', 'firstname4', 'last_name4', 'login4', 'patronymic4', 'squad_role4', 'state4', 1);
+values (4, 'test_email4@email.com', 'firstname4', 'last_name4', 'login4', 'patronymic4', 'CANDIDATE', 'BASIC_STATE', 1);
