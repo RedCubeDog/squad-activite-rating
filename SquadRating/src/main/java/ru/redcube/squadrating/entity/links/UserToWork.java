33d8de89c94bfbb0e4116ad6e36b6227d3fa9f34
redcube <test_ac_1@mail.ru>
2023-04-18 01:31:01 +0500

    Changed UserToWork

--- SquadRating/src/main/java/ru/redcube/squadrating/entity/links/UserToWork.java
+++ SquadRating/src/main/java/ru/redcube/squadrating/entity/links/UserToWork.java
@@ -13,7 +13,7 @@ public abstract class UserToWork {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @Column(name = "work_id")
     private Long workId;
