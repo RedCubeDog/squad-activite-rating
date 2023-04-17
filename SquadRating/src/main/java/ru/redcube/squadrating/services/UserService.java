07872ca42d468918223f3919fac09bfb68ea25a6
vudovenko <vladislav.udovenko0@yandex.ru>
2023-04-17 23:32:42 +0500

    Created UserService

---
+++ SquadRating/src/main/java/ru/redcube/squadrating/services/UserService.java
@@ -0,0 +1,23 @@
+package ru.redcube.squadrating.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.redcube.squadrating.dao.UserDAO;
+import ru.redcube.squadrating.entity.user.User;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserDAO userDAO;
+
+    @Autowired
+    public UserService(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    public List<User> getUsers() {
+        return userDAO.findAll();
+    }
+}
