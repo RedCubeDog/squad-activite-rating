17d62d5a2b1ce55f5aa0b546bcb0f2ce6c63969f
vudovenko <vladislav.udovenko0@yandex.ru>
2023-04-17 23:33:29 +0500

    MainPageController implemented

--- SquadRating/src/main/java/ru/redcube/squadrating/controllers/MainPageController.java
+++ SquadRating/src/main/java/ru/redcube/squadrating/controllers/MainPageController.java
@@ -1,13 +1,28 @@
 package ru.redcube.squadrating.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.redcube.squadrating.services.SquadService;
+import ru.redcube.squadrating.services.UserService;
 
 @Controller
 public class MainPageController {
 
+    private final UserService userService;
+    private final SquadService squadService;
+
+    @Autowired
+    public MainPageController(UserService userService, SquadService squadService) {
+        this.userService = userService;
+        this.squadService = squadService;
+    }
+
     @GetMapping("/")
-    public String getMainPage() {
+    public String getMainPage(Model model) {
+        model.addAttribute("users", userService.getUsers());
+        model.addAttribute("squads", squadService.getSquads());
         return "/index";
     }
 }
