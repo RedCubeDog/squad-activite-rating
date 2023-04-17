69c0776846d3994814b74a031ffd111dd04361cf
vudovenko <vladislav.udovenko0@yandex.ru>
2023-04-17 23:30:37 +0500

    Changed SquadDAO

--- SquadRating/src/main/java/ru/redcube/squadrating/dao/SquadDAO.java
+++ SquadRating/src/main/java/ru/redcube/squadrating/dao/SquadDAO.java
@@ -1,7 +1,7 @@
 package ru.redcube.squadrating.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.redcube.squadrating.entity.user.User;
+import ru.redcube.squadrating.entity.squad.SquadImpl;
 
-public interface SquadDAO extends JpaRepository<User,Long> {
+public interface SquadDAO extends JpaRepository<SquadImpl,Long> {
 }
