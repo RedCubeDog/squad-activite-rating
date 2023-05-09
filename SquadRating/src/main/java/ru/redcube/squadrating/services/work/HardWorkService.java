ec994d866ffb4424451415d4f3b8acfb6c625368
redcube <test_ac_1@mail.ru>
2023-05-10 02:30:52 +0500

    add CRUD for HardWork

--- SquadRating/src/main/java/ru/redcube/squadrating/services/work/HardWorkService.java
+++ SquadRating/src/main/java/ru/redcube/squadrating/services/work/HardWorkService.java
@@ -2,13 +2,14 @@ package ru.redcube.squadrating.services.work;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.redcube.squadrating.repositories.work.HardWorkRepository;
 import ru.redcube.squadrating.entity.work.HardWork;
 
 import java.util.List;
 
 @Service
-public class HardWorkService {
+public class HardWorkService implements WorkService<HardWork> {
     private final HardWorkRepository hardWorkRepository;
 
     @Autowired
@@ -16,7 +17,29 @@ public class HardWorkService {
         this.hardWorkRepository = hardWorkRepository;
     }
 
-    public List<HardWork> getHardWorks() {
+
+    @Transactional
+    @Override
+    public List<HardWork> getAllWorks() {
         return hardWorkRepository.findAll();
     }
+
+    @Transactional
+    @Override
+    public HardWork getWork(Long id) {
+        return hardWorkRepository.getReferenceById(id);
+    }
+
+    @Transactional
+    @Override
+    public void deleteWork(Long id) {
+        hardWorkRepository.delete(getWork(id));
+    }
+
+    @Transactional
+    @Override
+    public void saveWork(HardWork hardWork) {
+        hardWorkRepository.save(hardWork);
+    }
+
 }
