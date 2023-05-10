7afcb2f1a6220e9d8e4c1dce604eb8a679ccb0b0
vudovenko <vladislav.udovenko0@yandex.ru>
2023-05-10 21:12:25 +0500

    Added the display of the working hours of the units

---
+++ SquadRating/src/main/java/ru/redcube/squadrating/entity/converters/DurationAttributeConverter.java
@@ -0,0 +1,70 @@
+package ru.redcube.squadrating.entity.converters;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.time.Duration;
+
+@Converter(autoApply = true)
+public class DurationAttributeConverter implements AttributeConverter<Duration, String> {
+
+    public static String getFormattedTimeWork(Duration timeWork) {
+        return timeWork.getSeconds() / 3600 + "Ч: "
+                + timeWork.getSeconds() % 3600 / 60 + "М: "
+                + timeWork.getSeconds() % 60 + "С";
+    }
+
+    @Override
+    public String convertToDatabaseColumn(Duration duration) {
+        if (duration == null) {
+            return null;
+        }
+        long seconds = duration.getSeconds();
+        long nanos = duration.getNano();
+        boolean negative = duration.isNegative();
+
+        long absSeconds = Math.abs(seconds);
+        int hours = (int) (absSeconds / 3600);
+        int minutes = (int) ((absSeconds % 3600) / 60);
+        int remainingSeconds = (int) (absSeconds % 60);
+
+        String result = String.format("%s%d:%02d:%02d", negative ? "-" : "", hours, minutes, remainingSeconds);
+        if (nanos > 0) {
+            result += String.format(".%09d", nanos);
+        }
+        return result;
+    }
+
+    @Override
+    public Duration convertToEntityAttribute(String dbData) {
+        if (dbData == null) {
+            return null;
+        }
+
+        boolean negative = dbData.startsWith("-");
+        if (negative) {
+            dbData = dbData.substring(1);
+        }
+
+        String[] parts = dbData.split("\\.");
+        String timePart = parts[0];
+        String[] timeParts = timePart.split(":");
+        int hours = Integer.parseInt(timeParts[0]);
+        int minutes = Integer.parseInt(timeParts[1]);
+        int seconds = Integer.parseInt(timeParts[2]);
+
+        long totalSeconds = hours * 3600L + minutes * 60L + seconds;
+        long nanos = 0;
+        if (parts.length > 1) {
+            String nanosPart = parts[1];
+            nanos = Long.parseLong(nanosPart);
+            int numDigits = nanosPart.length();
+            if (numDigits < 9) {
+                int scalingFactor = (int) Math.pow(10, 9 - numDigits);
+                nanos *= scalingFactor;
+            }
+        }
+
+        return Duration.ofSeconds(negative ? -totalSeconds : totalSeconds, nanos);
+    }
+}
--- SquadRating/src/main/resources/data.sql
+++ SquadRating/src/main/resources/data.sql
@@ -1,36 +1,54 @@
 insert into squad (id, name)
-values (1, 'squad1');
+values (1, 'squad1'),
+       (2, 'squad2'),
+       (3, 'squad3');
 
 insert into app_user (id, email, first_name, last_name, login, patronymic, squad_role, state, squad_id)
 values (1, 'test_email1@email.com', 'firstname1', 'last_name1', 'login1', 'patronymic1', 'CANDIDATE', 'BASIC_STATE', 1),
-(2, 'test_email2@email.com', 'firstname2', 'last_name2', 'login2', 'patronymic2', 'CANDIDATE', 'BASIC_STATE', 1),
-(3, 'test_email3@email.com', 'firstname3', 'last_name3', 'login3', 'patronymic3', 'CANDIDATE', 'BASIC_STATE', 1),
-(4, 'test_email4@email.com', 'firstname4', 'last_name4', 'login4', 'patronymic4', 'CANDIDATE', 'BASIC_STATE', 1);
+       (2, 'test_email2@email.com', 'firstname2', 'last_name2', 'login2', 'patronymic2', 'CANDIDATE', 'BASIC_STATE', 1),
+       (3, 'test_email3@email.com', 'firstname3', 'last_name3', 'login3', 'patronymic3', 'CANDIDATE', 'BASIC_STATE', 2),
+       (4, 'test_email4@email.com', 'firstname4', 'last_name4', 'login4', 'patronymic4', 'CANDIDATE', 'BASIC_STATE', 2),
+       (5, 'test_email5@email.com', 'firstname5', 'last_name5', 'login5', 'patronymic5', 'CANDIDATE', 'BASIC_STATE', 3),
+       (6, 'test_email6@email.com', 'firstname6', 'last_name6', 'login6', 'patronymic6', 'CANDIDATE', 'BASIC_STATE', 3);
 
 insert into hard_work (id, title, description, date, coefficient)
 values (1, 'HardWork1', 'make something 1', '2022-09-19', 1),
-(2, 'HardWork2', 'make something 1', '2022-10-22', 1),
-(3, 'HardWork2', 'make something 2', '2022-10-19', 1),
-(4, 'HardWork3', 'make something 2', '2022-11-22', 1);
+       (2, 'HardWork2', 'make something 2', '2022-10-22', 1),
+       (3, 'HardWork3', 'make something 3', '2022-10-19', 2),
+       (4, 'HardWork4', 'make something 4', '2022-11-22', 2),
+       (5, 'HardWork5', 'make something 5', '2022-07-22', 1),
+       (6, 'HardWork6', 'make something 6', '2022-05-12', 1);
 
 insert into social_work (id, title, description, date, coefficient)
-values (1, 'SocialWork1', 'make something 4', '2022-10-23', 1),
-(2, 'SocialWork2', 'make something 5', '2022-12-22', 1),
-(3, 'SocialWork2', 'make something 6', '2022-10-24', 1),
-(4, 'SocialWork3', 'make something 6', '2022-11-11', 1);
+values (1, 'SocialWork1', 'make something 1', '2022-08-19', 1),
+       (2, 'SocialWork2', 'make something 2', '2022-09-22', 1),
+       (3, 'SocialWork3', 'make something 3', '2022-09-19', 1),
+       (4, 'SocialWork4', 'make something 4', '2022-10-22', 1),
+       (5, 'SocialWork5', 'make something 5', '2022-06-22', 1),
+       (6, 'SocialWork6', 'make something 6', '2022-03-12', 1);
 
-insert into user_to_hard_work (id, work_Id, user_Id)
-values (1, 1, 1),
-(2, 1, 2),
-(3, 2, 1),
-(4, 3, 3),
-(5, 4, 2),
-(6, 4, 4);
+INSERT INTO user_to_hard_work (id, work_id, user_id, time_work)
+VALUES (1, 1, 1, '01:00:00'),
+       (2, 2, 1, '01:35:37'),
+       (3, 2, 2, '00:03:11'),
+       (4, 3, 3, '00:45:37'),
+       (5, 3, 4, '07:30:00'),
+       (6, 4, 4, '00:27:00'),
+       (7, 6, 5, '17:01:12'),
+       (8, 5, 6, '00:30:57');
 
-insert into user_to_social_work (id, work_Id, user_Id)
-values (1, 1, 1),
-(2, 2, 1),
-(3, 2, 2),
-(4, 3, 2),
-(5, 4, 3),
-(6, 4, 3);
+INSERT INTO user_to_social_work (id, work_id, user_id, time_work)
+VALUES (1, 1, 1, '07:00:00'),
+       (2, 2, 1, '00:38:20'),
+       (3, 2, 2, '03:27:00'),
+       (4, 3, 3, '00:23:30'),
+       (5, 3, 4, '04:18:00'),
+       (6, 4, 4, '00:28:00'),
+       (7, 6, 5, '06:07:12'),
+       (8, 5, 6, '01:48:56');
+
+INSERT INTO mg_users
+VALUES (1, true, '', '', 123, user);
+
+INSERT INTO user_role (user_id, roles)
+VALUES (1, 'USER');
