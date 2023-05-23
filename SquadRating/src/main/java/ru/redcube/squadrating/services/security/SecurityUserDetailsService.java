37a16a40a0171c793255a4ae19a306b7bc595c52
vudovenko <vladislav.udovenko0@yandex.ru>
2023-05-24 00:36:27 +0500

    Deleted my own implementation of UserDetails

--- SquadRating/src/main/java/ru/redcube/squadrating/services/security/SecurityUserDetailsService.java
+++ SquadRating/src/main/java/ru/redcube/squadrating/services/security/SecurityUserDetailsService.java
@@ -1,27 +1,40 @@
 package ru.redcube.squadrating.services.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import ru.redcube.squadrating.configs.SecurityUserDetails;
 import ru.redcube.squadrating.entity.security.SecurityUser;
 import ru.redcube.squadrating.repositories.security.SecurityUserRepository;
 
+import java.util.Arrays;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
-public class  SecurityUserDetailsService implements UserDetailsService {
+public class SecurityUserDetailsService implements UserDetailsService {
 
     @Autowired
     private SecurityUserRepository securityUserRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<SecurityUser> securityUser = securityUserRepository.findByUsername(username);
-        return securityUser.map(SecurityUserDetails::new)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        Optional<SecurityUser> optional = securityUserRepository.findByUsername(username);
+        if (optional.isEmpty()) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return mapRolesToAuthorities(optional.get());
+    }
+
+    private static User mapRolesToAuthorities(SecurityUser securityUser) {
+        return new User(securityUser.getUsername(), securityUser.getPassword(),
+                Arrays
+                        .stream(securityUser.getRoles().split(", "))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList()));
     }
 
     public void addUser(SecurityUser securityUser) throws Exception {
